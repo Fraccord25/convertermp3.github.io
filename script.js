11b5a14498b6ecb9b999f95e47f84d5d10594ba2
Fajar Ramadhan <114432075+Fraccord25@users.noreply.github.com>
2025-07-17 22:43:38 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+async function convert() {
+  const url = document.getElementById('ytUrl').value;
+  const videoId = extractVideoId(url);
+
+  if (!videoId) {
+    alert("Link YouTube tidak valid!");
+    return;
+  }
+
+  const options = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': 'c7f82162dcmsh76d3b3b9660a051p1f3d40jsn261ff6fa6f17',
+      'X-RapidAPI-Host': 'youtube-mp36.p.rapidapi.com'
+    }
+  };
+
+  try {
+    const res = await fetch(`https://youtube-mp36.p.rapidapi.com/dl?id=${videoId}`, options);
+    const data = await res.json();
+
+    if (data.status === "ok") {
+      document.getElementById('result').innerHTML = `
+        <p><strong>${data.title}</strong></p>
+        <a href="${data.link}" target="_blank">⬇️ Download MP3</a>
+      `;
+    } else {
+      document.getElementById('result').textContent = 'Gagal mengunduh.';
+    }
+  } catch (err) {
+    console.error(err);
+    alert("Terjadi kesalahan saat menghubungi API.");
+  }
+}
+
+function extractVideoId(url) {
+  const regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=)([^#&?]*).*/;
+  const match = url.match(regExp);
+  return match && match[2].length === 11 ? match[2] : null;
+}
